4a07da051e5bc4bfa6e7fe422df82f00a3018f48
BadriRokhvadze <166226165+BadriRokhvadze@users.noreply.github.com>
2024-04-07 23:51:46 +0400

    Add files via upload

--- day 1/lesson1.py
+++ day 1/lesson1.py
@@ -31,7 +31,9 @@ penup()
 goto(-100,-200)
 pendown()
 
-color("black")
+color("red")
+
+begin_fill()
 
 forward(250)
 left(90)
@@ -41,6 +43,8 @@ forward(250)
 left(90)
 forward(250)
 left(90)
+
+end_fill()
  #house_walls_end
 
 
@@ -49,6 +53,8 @@ left(90)
 penup()
 goto(150,50)
 pendown()
+
+color("black")
 
 begin_fill()
 
@@ -77,10 +83,52 @@ forward(100)
 end_fill()
  #house_door_end
 
+ #house_window_frames
+penup()
+goto(-75,-50)
+pendown()
+
+color("white")
+
+begin_fill()
+
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+
+end_fill()
+
+penup()
+goto(75,-50)
+pendown()
+
+begin_fill()
+
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+
+end_fill()
+ #house_window_frames_end
+
+
+
  #house_windows_1
 penup()
 goto(-75,-50)
 pendown()
+
+color("black")
 
 left(90)
 forward(25)
@@ -148,12 +196,55 @@ forward(170)
 end_fill()
  #house_chimney_end
 
+ ###
+penup()
+goto(150,50)
+pendown()
+
+right(90)
+forward(250)
+
+penup()
+goto(0,-200)
+pendown()
+
+right(180)
+forward(50)
+ ###
+
+ #house_chimney_smoke
+penup()
+goto(100,235)
+pendown()
+
+color("white")
+
+begin_fill()
+
+circle(radius=20)
+
+penup()
+goto(100,275)
+pendown()
+
+circle(radius=25)
+
+penup()
+goto(100,325)
+pendown()
+
+circle(radius=30)
+
+end_fill()
+ #house_chimney_smoke_end
+
+
  #house_door_handle
 penup()
 goto(40,-150)
 pendown()
 
-color("gray")
+color("red")
 
 shape("circle")
 
