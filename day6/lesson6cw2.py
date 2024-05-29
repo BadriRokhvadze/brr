fa0f38505cfb7ac5f622d28fd51c943f040e7b89
BadriRokhvadze <166226165+BadriRokhvadze@users.noreply.github.com>
2024-05-29 23:57:35 +0400

    Add files via upload

---
+++ day6/lesson6cw2.py
@@ -0,0 +1,13 @@
+user_first_name = input('Please enter your firstname: ' )
+user_last_name = input('Please enter your lastname: ')
+user_age = input('Please enter your age: ')
+user_address = input('Please enter your address: ')
+user_email = input('Please enter your email: ')
+
+
+print('Your inputed information is')
+print('Your first name:',user_first_name)
+print('Your last name:',user_last_name)
+print('Your age:',user_age)
+print('Your address:',user_address)
+print('Your email:',user_email)
---
+++ day6/lesson6hw2.py
@@ -0,0 +1,19 @@
+me = int(input('Please enter your age: '))
+dad = int(input('Please enter your fathers age: '))
+mom = int(input('Please enter your mothers age: '))
+sibling = int(input('Please enter your siblings age if you have one: '))
+
+print(' ')
+
+print('Your age:',me)
+print('Your age in 20 years',me+20)
+print(' ')
+print('Your fathers age:',dad)
+print('Your fathers age in 20 years:',dad+20)
+print(' ')
+print('Your mothers age:',mom)
+print('Your mothers age in 20 years:',mom+20)
+print(' ')
+print('Your siblings age:',sibling)
+print('Your siblings age in 20 years:',sibling+20)
+
