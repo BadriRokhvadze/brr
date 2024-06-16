0407e4242e0af9686a1b294aa77296e6331292ee
BadriRokhvadze <166226165+BadriRokhvadze@users.noreply.github.com>
2024-06-16 12:06:19 +0400

    Add files via upload

---
+++ lesson10cw1.py
@@ -0,0 +1,9 @@
+enter_pass = input('Please enter your password: ')
+
+user_pass = ('luka1234')
+
+
+if enter_pass==user_pass:
+  print('You have passed the authorization.')
+else:
+  print('The password is incorrect try again.')
