4a9f56f7ab093206ad78eb08a95e010c9ef9ef13
BadriRokhvadze <166226165+BadriRokhvadze@users.noreply.github.com>
2024-06-16 15:31:26 +0400

    Add files via upload

---
+++ day10/Homework/lesson10hw2.py
@@ -0,0 +1,8 @@
+o = int(input('Please enter a number: '))
+
+for i in range(0,o):
+    n = 0 + i
+
+print(n)
+
+#couldnt figure it out
---
+++ day10/Homework/lesson10hw3.py
@@ -0,0 +1,10 @@
+num1 = int(input("please enter number: "))
+num2 = int(input("please enter number: "))
+sum_number = num1 + num2
+
+for i in range(1,21):
+    result = sum_number * i
+
+print(f"{sum_number} * {i} = {result}")     
+
+#couldnt figure out this one either
