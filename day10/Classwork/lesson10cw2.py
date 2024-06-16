65a104597483d53de87164de480e0f5768d7bae7
BadriRokhvadze <166226165+BadriRokhvadze@users.noreply.github.com>
2024-06-16 12:07:16 +0400

    Add files via upload

---
+++ day10/Classwork/lesson10cw2.py
@@ -0,0 +1,7 @@
+sum = 1
+
+for i in range(5):
+    num = int(input('Please enter a number: '))
+    sum = sum * num
+
+print(sum)
---
+++ day10/Classwork/lesson10cw3.py
@@ -0,0 +1,6 @@
+o = 0
+
+for i in range(6,16):
+    o = o + i
+
+print(o)
