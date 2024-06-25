d1c9fb71f439782d8f22fe1be388722fda6fd27e
BadriRokhvadze <166226165+BadriRokhvadze@users.noreply.github.com>
2024-06-26 00:55:55 +0400

    Add files via upload

---
+++ lesson12cw1.py
@@ -0,0 +1,5 @@
+o = 20
+
+while o > 0:
+    print(o)
+    o = o - 1
---
+++ lesson12hw1.py
@@ -0,0 +1,16 @@
+o = int(input('PLease enter a number from 1 to 7: '))
+
+if o == 1:
+    print('Monday')
+elif o == 2:
+    print('Tuesday')
+elif o == 3:
+    print('Wednesday')
+elif o == 4:
+    print('Thursday')
+elif o == 5:
+    print('Friday')
+elif o == 6:
+    print('Saturday')
+elif o == 7:
+    print('Sunday')
